c7e16147025aa2e173e29834c9ed0e6eaee13d0e
Guilliano van Dinther <guilliano.vandinther@ictmbo.nl>
2023-10-18 13:30:31 +0200

    update

--- nvim/lua/main/init.lua
+++ nvim/lua/main/init.lua
@@ -1,2 +1,13 @@
 require("main.remap")
 require("main.set")
+
+require('telescope').setup{ 
+    defaults = { 
+        file_ignore_patterns = { 
+            "node_modules",
+            ".git",
+            "vendor",
+            ".idea",
+        }
+    }
+}
--- nvim/lua/main/set.lua
+++ nvim/lua/main/set.lua
@@ -8,3 +8,6 @@ vim.opt.expandtab = true
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
+
+vim.opt.swapfile = false
+vim.opt.writebackup = false
